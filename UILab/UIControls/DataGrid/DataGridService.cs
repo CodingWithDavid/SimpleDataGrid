e46fc4b311fe75e8dd9da02bb2b148d97201990e
David Gallivan <33528347+CodingWithDavid@users.noreply.github.com>
2020-01-28 07:58:55 -0700

    Initial Load

---
+++ UILab/UIControls/DataGrid/DataGridService.cs
@@ -0,0 +1,23 @@
+﻿
+namespace UILab.UIControls.DataGrid
+{
+    public class DataGridService
+    {
+        public static string GetSortStyle(int index, int sortIndex, bool sortDirection)
+        {
+            string result = string.Empty;
+            if (index == sortIndex)
+            {
+                if (sortDirection)
+                {
+                    result = "fa-sort-up";
+                }
+                else
+                {
+                    result = "fa-sort-down";
+                }
+            }
+            return result;
+        }
+    }
+}
